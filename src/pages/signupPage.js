c724996d6991ca505b7ac1e94a8c442bb5870ab2
swarnimvarchaswa <2021031178@mmmut.ac.in>
2024-10-25 11:09:19 +0530

    commit

--- src/pages/signupPage.js
+++ src/pages/signupPage.js
@@ -125,7 +125,7 @@ function SignupPage() {
     <div>
       {/* <div className=" absolute top-0 bottom-0 w-dvw bg-gradient-to-b from-50% from-purple-500  to-purple-200 opacity-30"></div> */}
 
-      <div className="absolute top-0 bottom-0 left-0 right-0 bg-purple-50">
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-purple-50 max-w-md mx-auto">
         {/* logo img */}
         <img
           className="w-[50%] mx-[25%] mt-[10%]"
